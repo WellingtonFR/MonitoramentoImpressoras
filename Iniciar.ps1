632ebbe419b1db3f4d2badb982e5de9bc61efaac
WellingtonFR <wellingtondfr@gmail.com>
2021-08-12 14:29:27 -0300

    handlebars

---
+++ Iniciar.ps1
@@ -0,0 +1,4 @@
+Set-Location $PSScriptRoot
+
+Start-Process "http://localhost:3000/"
+npm start
--- index.js
+++ index.js
@@ -1,32 +1,89 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
+const path = require("path");
+const csv = require("csv-parser");
+const hbs = require("express-handlebars");
+const fs = require("fs");
+const { html } = require("cheerio/lib/api/manipulation");
+const { title } = require("process");
+const { urlToHttpOptions } = require("url");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 //express
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+app.use(express.static(__dirname + "/public"));
 
-const url = "https://10.50.242.22/";
-const tabela = [];
+//handlebars
+var hbshelper = hbs.create({
+  helpers: {
+    if: function (v1, v2, options) {
+      if (v1 == v2) {
+        return options.fn(this);
+      } else {
+        return options.inverse(this);
+      }
+    },
+  },
+});
+
+app.engine("handlebars", hbshelper.engine);
+app.set("view engine", "handlebars");
+
+let dados = [];
+let tabela = [];
+
+fs.createReadStream("Impressoras.csv")
+  .pipe(csv())
+  .on("data", (data) => dados.push(data));
+
+function recuperaInformacoes() {
+  tabela = [];
+  dados.forEach(async (dado) => {
+    let url = "http://" + dado.IP;
+    await axios(url)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        let TonerBruto = $("#SupplyPLR0").text();
+        let Toner = TonerBruto.substring(0, TonerBruto.length - 2);
+        let KitDeManutencaoBruto = $("#SupplyPLR1").text();
+        let KitDeManutencao = KitDeManutencaoBruto.substring(
+          0,
+          KitDeManutencaoBruto.length - 2
+        );
+        let Modelo = $("#HomeDeviceName").text();
+
+        if (KitDeManutencao == "") {
+          KitDeManutencao = "Não possui";
+        }
+
+        if (Toner == "<10") {
+          Toner = Toner.substring(1, Toner.length);
+        }
+
+        if (!Modelo == "") {
+          tabela.push({
+            Fila: dado.Fila,
+            IP: dado.IP,
+            Modelo: Modelo,
+            Toner: Toner,
+            KitDeManutencao: KitDeManutencao,
+          });
+        }
+      })
+      .catch((err) => {});
+  });
+}
 
 app.get("/", (req, res) => {
-  axios(url).then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const Toner = $("#SupplyPLR0").text();
-    const KitDeManutencao = $("#SupplyPLR1").text();
+  recuperaInformacoes();
+  res.render("home");
+});
 
-    tabela.push({ Toner: Toner, KitDeManutencao: KitDeManutencao });
-
-    res.send(
-      "Hello world"
-      //   "Impressora IMP_094_FAT_REL_01 com kit de manutencao em " +
-      //     KitDeManutencao +
-      //     " e toner em " +
-      //     Toner
-    );
-  });
+app.get("/info", (req, res) => {
+  res.render("info", { tabela: tabela });
 });
 
 app.listen(port, () => {});
